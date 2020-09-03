1184268a9c29749f8981f5754294396e37b80537
Bogdan Bis <bogdan12167@gmail.com>
2020-09-03 17:11:58 +0300

    added Sponsorizari api on /sponsorizari

---
+++ src/main/java/net/guides/springboot2/crud/repository/SponsorizariRepository.java
@@ -0,0 +1,9 @@
+package net.guides.springboot2.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.guides.springboot2.crud.model.Sponsorizari;
+
+@Repository
+public interface SponsorizariRepository extends JpaRepository<Sponsorizari, Long>{ }
