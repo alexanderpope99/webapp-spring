25ac7ec34060bcff266732f8b264534e8d60bfa3
Bogdan Bis <bogdan12167@gmail.com>
2020-09-03 17:33:07 +0300

    Added StatSalariat backend on /statsalariat

---
+++ src/main/java/net/guides/springboot2/crud/model/StatSalariat.java
@@ -0,0 +1,53 @@
+package net.guides.springboot2.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "prime")
+public class StatSalariat {
+
+    private long id;
+    @Column(name = "data")
+    private String data;
+    @Column(name = "idcontract")
+    private Long idcontract;
+    
+
+    public StatSalariat() { }
+
+    public StatSalariat( String data, Long idcontract ) {
+        this.data = data;
+        this.idcontract = idcontract;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // GETTERS
+    public String getData() {
+        return data;
+    }
+    public Long getIdcontract() {
+        return idcontract;
+    }
+
+    // SETTERS
+    public void setData(String data) {
+        this.data = data;
+    }
+    public void setIdcontract(Long idcontract) {
+        this.idcontract = idcontract;
+    }
+}
+
