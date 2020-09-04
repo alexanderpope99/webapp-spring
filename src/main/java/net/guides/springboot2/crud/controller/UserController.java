a3b73bfbeade8fd0ae6d608148eac1b2cf17c37c
Bogdan Bis <bogdan12167@gmail.com>
2020-09-04 12:29:25 +0300

    Added User api on /user

---
+++ src/main/java/net/guides/springboot2/crud/controller/UserController.java
@@ -0,0 +1,69 @@
+package net.guides.springboot2.crud.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.guides.springboot2.crud.exception.ResourceNotFoundException;
+import net.guides.springboot2.crud.model.User;
+import net.guides.springboot2.crud.repository.UserRepository;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllPersoane() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value="id") Long id) throws ResourceNotFoundException
+    {
+        User user = userRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+        public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id,  @RequestBody User newUser) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+
+        newUser.setId(user.getId());
+        final User updatedUser = userRepository.save(newUser);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
---
+++ src/main/java/net/guides/springboot2/crud/model/User.java
@@ -0,0 +1,79 @@
+package net.guides.springboot2.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    private long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "nume")
+    private String nume;
+    @Column(name = "prenume")
+    private String prenume;
+    @Column(name = "societateselectata")
+    private Long societateselectata;
+    
+    public User() { }
+
+    public User( String username, String password, String nume, String prenume, Long societateselecteta ) {
+        this.username = username;
+        this.password = password;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.societateselectata = societateselecteta;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // GETTERS
+    public String getNume() {
+        return nume;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getPrenume() {
+        return prenume;
+    }
+    public Long getSocietateselectata() {
+        return societateselectata;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    // SETTERS
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+    public void setSocietateselectata(Long societateselectata) {
+        this.societateselectata = societateselectata;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+} 
+
