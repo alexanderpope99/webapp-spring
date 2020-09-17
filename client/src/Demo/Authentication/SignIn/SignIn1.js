db183f74c99030d750e1053f9d5590fc1fe946fd
Alexandru Pop <apop2299@gmail.com>
2020-09-17 11:41:15 +0300

    Enter on Form

--- client/src/Demo/Authentication/SignIn/SignIn1.js
+++ client/src/Demo/Authentication/SignIn/SignIn1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 
 import './../../../assets/scss/style.scss';
 import Aux from '../../../hoc/_Aux';
@@ -18,13 +18,10 @@ class SignUp1 extends React.Component {
     };
   }
   async handleClick() {
-    await fetch(
-      `${server.address}/user/usr=${this.state.username}/pass=${this.state.password}`,
-      {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      }
-    )
+    await fetch(`${server.address}/user/usr=${this.state.username}/pass=${this.state.password}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
       .then((res) => {
         if (res.ok) {
           sessionStorage.setItem('logged', true);
@@ -41,92 +38,79 @@ class SignUp1 extends React.Component {
   render() {
     return (
       <Aux>
-        <Modal
-          show={this.state.show}
-          onHide={() => this.setState({ show: false })}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Utilizator invalid</Modal.Title>
-          </Modal.Header>
-          <Modal.Footer>
-            <Button
-              variant="primary"
-              onClick={() => this.setState({ show: false })}
-            >
-              OK
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        <Breadcrumb />
-        <div className="auth-wrapper">
-          <div className="auth-content">
-            <div className="auth-bg">
-              <span className="r" />
-              <span className="r s" />
-              <span className="r s" />
-              <span className="r" />
-            </div>
-            <div className="card">
-              <div className="card-body text-center">
-                <div className="mb-4">
-                  <i className="feather icon-unlock auth-icon" />
-                </div>
-                <h3 className="mb-4">Login</h3>
-                <div className="input-group mb-3">
-                  <input
-                    onChange={(e) =>
-                      this.setState({
-                        username: e.target.value,
-                      })
-                    }
-                    type="username"
-                    className="form-control"
-                    placeholder="Username"
-                  />
-                </div>
-                <div className="input-group mb-4">
-                  <input
-                    onChange={(e) =>
-                      this.setState({
-                        password: e.target.value,
-                      })
-                    }
-                    type="password"
-                    className="form-control"
-                    placeholder="password"
-                  />
-                </div>
-                <div className="form-group text-left">
-                  <div className="checkbox checkbox-fill d-inline">
-                    <input
-                      type="checkbox"
-                      name="checkbox-fill-1"
-                      id="checkbox-fill-a1"
-                    />
-                    <label htmlFor="checkbox-fill-a1" className="cr">
-                      {' '}
-                      Save credentials
-                    </label>
+        <Form onSubmit={this.handleClick}>
+          <Modal show={this.state.show} onHide={() => this.setState({ show: false })}>
+            <Modal.Header closeButton>
+              <Modal.Title>Utilizator invalid</Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+              <Button variant="primary" onClick={() => this.setState({ show: false })}>
+                OK
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <Breadcrumb />
+          <div className="auth-wrapper">
+            <div className="auth-content">
+              <div className="auth-bg">
+                <span className="r" />
+                <span className="r s" />
+                <span className="r s" />
+                <span className="r" />
+              </div>
+              <div className="card">
+                <div className="card-body text-center">
+                  <div className="mb-4">
+                    <i className="feather icon-unlock auth-icon" />
                   </div>
+                  <h3 className="mb-4">Login</h3>
+                  <div className="input-group mb-3">
+                    <input
+                      onChange={(e) =>
+                        this.setState({
+                          username: e.target.value,
+                        })
+                      }
+                      type="username"
+                      className="form-control"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div className="input-group mb-4">
+                    <input
+                      onChange={(e) =>
+                        this.setState({
+                          password: e.target.value,
+                        })
+                      }
+                      type="password"
+                      className="form-control"
+                      placeholder="password"
+                    />
+                  </div>
+                  <div className="form-group text-left">
+                    <div className="checkbox checkbox-fill d-inline">
+                      <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-a1" />
+                      <label htmlFor="checkbox-fill-a1" className="cr">
+                        {' '}
+                        Save credentials
+                      </label>
+                    </div>
+                  </div>
+                  <button type="submit" className="btn btn-primary shadow-2 mb-4">
+                    Login
+                  </button>
+                  <p className="mb-2 text-muted">
+                    Forgot password? <NavLink to="/auth/reset-password-1">Reset</NavLink>
+                  </p>
+                  <p className="mb-0 text-muted">
+                    Don’t have an account? <NavLink to="/auth/signup-1">Signup</NavLink>
+                  </p>
                 </div>
-                <button
-                  onClick={this.handleClick}
-                  className="btn btn-primary shadow-2 mb-4"
-                >
-                  Login
-                </button>
-                <p className="mb-2 text-muted">
-                  Forgot password?{' '}
-                  <NavLink to="/auth/reset-password-1">Reset</NavLink>
-                </p>
-                <p className="mb-0 text-muted">
-                  Don’t have an account?{' '}
-                  <NavLink to="/auth/signup-1">Signup</NavLink>
-                </p>
               </div>
             </div>
           </div>
-        </div>
+        </Form>
       </Aux>
     );
   }
