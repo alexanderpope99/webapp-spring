1be99461854c10736929ade8b64572444e0e29ec
Alexandru Pop <apop2299@gmail.com>
2020-10-01 10:39:35 +0300

    UserJPA update

--- server/src/main/java/net/guides/springboot2/crud/model/User.java
+++ server/src/main/java/net/guides/springboot2/crud/model/User.java
@@ -29,6 +29,10 @@ public class User {
 	@Size(max = 120)
 	private String password;
 
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_angajat", referencedColumnName = "idpersoana")
+	private Angajat id_angajat;
+
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
