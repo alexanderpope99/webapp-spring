3c7ccdd2f72a99fa31c19e895133f7d867584f53
bogdanbis <bogdan12167@gmail.com>
2020-10-05 14:11:10 +0300

    starting Change Password

--- client/src/Demo/Edit/Profile.js
+++ client/src/Demo/Edit/Profile.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { Row, Col, Card, Form, Button, Modal } from 'react-bootstrap';
 
 import Aux from '../../hoc/_Aux';
 
 export default class Profile extends React.Component {
+  4;
+  /*
+   *	user can change his email or password
+   */
   constructor() {
     super();
     this.changeEdit = this.changeEdit.bind(this);
-		this.getUserDetails = this.getUserDetails.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
+    this.getUserDetails = this.getUserDetails.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       id: null,
@@ -25,17 +30,29 @@ export default class Profile extends React.Component {
 
   getUserDetails() {
     const user = JSON.parse(localStorage.getItem('user'));
-    console.log(user);
     this.setState({
       id: user.id,
       username: user.username,
       email: user.email,
       roles: user.roles,
+
+      // modal:
+      show: false,
+			modalMessage: '',
+			parolaActuala: '',
+			parolaNoua: '',
+			parolaNouaCheck: ''
     });
   }
 
   changeEdit() {
     this.setState({ isEdit: !this.state.isEdit });
+  }
+  handleClose() {
+    this.setState({
+      show: false,
+      modalMessage: '',
+    });
   }
 
   getRoleName(role) {
@@ -58,16 +75,55 @@ export default class Profile extends React.Component {
   }
 
   onSubmit(e) {
-		e.preventDefault();
-		
-		// if(this.state.isEdit)
-		this.changeEdit();
+    e.preventDefault();
 
+    this.setState({
+      show: true,
+    });
   }
 
   render() {
     return (
       <Aux>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Mesaj</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label>Parola actuală</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={this.state.parolaActuala}
+                  onChange={(e) => this.setState({ parolaActuala: e.target.value })}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Parola actuală</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={this.state.parolaNoua}
+                  onChange={(e) => this.setState({ parolaNoua: e.target.value })}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Parola actuală</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={this.state.parolaNouaCheck}
+                  onChange={(e) => this.setState({ parolaNouaCheck: e.target.value })}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.handleClose}>
+              Schimbă
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         <Row>
           <Col>
             <Form onSubmit={this.onSubmit}>
@@ -75,12 +131,11 @@ export default class Profile extends React.Component {
                 <Card.Header>
                   <Card.Title as="h5">Profil</Card.Title>
                   <Button
-										type="submit"
-                    // onClick={this.onSubmit}
+                    type="submit"
                     variant={this.state.isEdit ? 'primary' : 'link'}
                     className="float float-right m-0 p-1"
                   >
-                    {this.state.isEdit ? 'Salvează' : 'Editează'}
+                    Schimbă parola
                   </Button>
                 </Card.Header>
                 <Card.Body>
@@ -95,7 +150,7 @@ export default class Profile extends React.Component {
                       />
                     </Form.Group>
                     <Form.Group as={Col} md="6">
-                      <Form.Label>email</Form.Label>
+                      <Form.Label>e-mail</Form.Label>
                       <Form.Control
                         type="text"
                         value={this.state.email}
