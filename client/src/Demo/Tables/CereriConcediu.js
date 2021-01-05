e7a4002f259a7389f1b084466bbd2b5186b20820
Alexandru Pop <apop2299@gmail.com>
2021-01-05 09:23:41 +0200

    CereriConcediu

--- client/src/Demo/Tables/CereriConcediu.js
+++ client/src/Demo/Tables/CereriConcediu.js
@@ -436,7 +436,9 @@ class CereriConcediuTabel extends React.Component {
           <Col>
             <Card>
               <Card.Header className="border-0">
-                <Card.Title as="h5">{this.state.socsel ? this.state.socsel.nume+' - ' : ''}Cereri Concediu</Card.Title>
+                <Card.Title as="h5">
+                  {this.state.socsel ? this.state.socsel.nume + ' - ' : ''}Cereri Concediu
+                </Card.Title>
 
                 <Button
                   variant="outline-info"
