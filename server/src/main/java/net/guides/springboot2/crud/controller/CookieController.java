8726b2c1d46dc407c2d2000905a1de04d88f22a6
Alexandru Pop <apop2299@gmail.com>
2021-01-19 15:11:49 +0200

    CookieController

--- server/src/main/java/net/guides/springboot2/crud/controller/CookieController.java
+++ server/src/main/java/net/guides/springboot2/crud/controller/CookieController.java
@@ -1,6 +1,7 @@
 package net.guides.springboot2.crud.controller;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.Cookie;
@@ -12,26 +13,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.CookieValue;
-
 @RestController
 @RequestMapping("/cookie")
 public class CookieController {
 
-	@GetMapping("/set/{key}/{value}")
+	@GetMapping("/add/{key}/{value}")
 	public String setCookie(@PathVariable("key") String key, @PathVariable("value") String value,
 			HttpServletResponse response) {
 		Cookie cookie = new Cookie(key, value);
+		cookie.setPath("/cookie/key=" + key);
+		cookie.setHttpOnly(true);
 		response.addCookie(cookie);
 		return "Cookie adăugat";
 	}
 
 	@GetMapping
-	public String readCookie(@CookieValue(value = "username", defaultValue = "Atta") String username) {
-		return "Hey! My username is " + username;
-	}
-
-	@GetMapping("/all")
 	public String readAllCookies(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
 		if (cookies != null)
@@ -39,12 +35,19 @@ public class CookieController {
 		return "No cookies";
 	}
 
-	@GetMapping("/delete/{name}")
-	public String deleteCookie(@PathVariable("name") String name, HttpServletResponse response) {
-		Cookie cookie = new Cookie(name, null);
+	@GetMapping("/key={key}")
+	public Optional<String> readCookies(HttpServletRequest request, @PathVariable("key") String key) {
+		return Arrays.stream(request.getCookies()).filter(c -> key.equals(c.getName())).map(Cookie::getValue).findAny();
+	}
+
+	@GetMapping("/delete/{key}")
+	public String deleteCookie(@PathVariable("key") String key, HttpServletResponse response) {
+		Cookie cookie = new Cookie(key, null);
 		cookie.setMaxAge(0);
+		cookie.setPath("/cookie/key=" + key);
+		cookie.setHttpOnly(true);
 		response.addCookie(cookie);
-		return "Gata";
+		return "Cookie șters";
 	}
 
 }
