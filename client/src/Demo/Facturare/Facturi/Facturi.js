3d8b918a2511f40eb546c07807bf7b764f3fa777
bogdanbis <bogdan12167@gmail.com>
2021-02-11 20:00:47 +0200

    g

--- client/src/Demo/Facturare/Facturi/Facturi.js
+++ client/src/Demo/Facturare/Facturi/Facturi.js
@@ -7,82 +7,85 @@ import EmitereFactura from './EmitereFactura';
 import { getSocSel } from '../../Resources/socsel';
 
 export default class Facturi extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.edit = this.edit.bind(this);
-    this.scrollToTopSmooth = this.scrollToTopSmooth.bind(this);
+		this.edit = this.edit.bind(this);
+		this.scrollToTopSmooth = this.scrollToTopSmooth.bind(this);
 
-    this.tabelFacturi = React.createRef();
+		this.tabelFacturi = React.createRef();
+		this.emiteFactura = React.createRef();
 
-    this.state = {
-      key: 'tabel-facturi',
-      socsel: getSocSel(),
+		this.state = {
+			key: 'tabel-facturi',
+			socsel: getSocSel(),
 
-      numarFactura: 0,
-    };
-  }
+			numarFactura: 0,
+		};
+	}
 
-  onSelectPill(key) {
-    if (key === 'tabel-facturi') {
-      this.tabelFacturi.current.getFacturi();
-    }
-    if (key === 'emite-factura') {
-      this.edit(null);
-    }
-    this.setState({ key: key, factura: null });
-  }
+	onSelectPill(key) {
+		if (key === 'tabel-facturi') {
+			this.tabelFacturi.current.getFacturi();
+			this.setState({ key: key, factura: null });
+		}
+		if (key === 'emite-factura') {
+			this.edit(null);
+		}
+	}
 
-  async edit(factura) {
-		console.log('emite factura:', factura);
-    this.setState({
-      key: 'emite-factura',
-      factura: factura,
-    });
-  }
+	async edit(factura) {
+		await this.emiteFactura.current.fillForm(factura);
+		this.setState({ key: 'emite-factura' });
+	}
 
-  scrollToTopSmooth() {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-  }
+	async cresteUltimulNumar() {
+		this.setState({ ultimulNumar: this.state.ultimulNumar + 1 })
+	}
 
-  render() {
-    return (
-      <Aux>
-        <Row>
-          <Col>
-            <Breadcrumb style={{ fontSize: '12px' }}>
-              <Breadcrumb.Item href="/dashboard/societati">
-                {this.state.socsel.nume}
-              </Breadcrumb.Item>
-              <Breadcrumb.Item active>Facturi</Breadcrumb.Item>
-            </Breadcrumb>
-            <Tabs
-              variant="pills"
-              activeKey={this.state.key}
-              onSelect={(key) => this.onSelectPill(key)}
-            >
-              <Tab eventKey="tabel-facturi" title="Tabel facturi">
-                <FacturiTabel
-                  ref={this.tabelFacturi}
-                  edit={this.edit}
-                  scrollToTopSmooth={this.scrollToTopSmooth}
-                />
-              </Tab>
-              <Tab eventKey="emite-factura" title="Emitere factură">
-                <EmitereFactura
-                  factura={this.state.factura}
-                  numarFactura={this.state.numarFactura}
-                  scrollToTopSmooth={this.scrollToTopSmooth}
-                />
-              </Tab>
-            </Tabs>
-          </Col>
-        </Row>
-      </Aux>
-    );
-  }
+	scrollToTopSmooth() {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	}
+
+	render() {
+		return (
+			<Aux>
+				<Row>
+					<Col>
+						<Breadcrumb style={{ fontSize: '12px' }}>
+							<Breadcrumb.Item href="/dashboard/societati">
+								{this.state.socsel.nume}
+							</Breadcrumb.Item>
+							<Breadcrumb.Item active>Facturi</Breadcrumb.Item>
+						</Breadcrumb>
+						<Tabs
+							variant="pills"
+							activeKey={this.state.key}
+							onSelect={(key) => this.onSelectPill(key)}
+						>
+							<Tab eventKey="tabel-facturi" title="Tabel facturi">
+								<FacturiTabel
+									ref={this.tabelFacturi}
+									edit={this.edit}
+									scrollToTopSmooth={this.scrollToTopSmooth}
+								/>
+							</Tab>
+							<Tab eventKey="emite-factura" title="Emitere factură">
+								<EmitereFactura
+									ref={this.emiteFactura}
+									factura={this.state.factura}
+									numarFactura={this.state.numarFactura}
+									scrollToTopSmooth={this.scrollToTopSmooth}
+								/>
+							</Tab>
+						</Tabs>
+					</Col>
+				</Row>
+			</Aux>
+		);
+	}
 }
