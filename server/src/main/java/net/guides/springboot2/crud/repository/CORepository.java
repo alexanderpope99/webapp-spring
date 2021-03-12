d4b3b54ae3abbc574492701e2cff73980b42a4c3
bogdanbis <bogdan12167@gmail.com>
2021-03-12 15:51:56 +0200

    concedii

--- server/src/main/java/net/guides/springboot2/crud/repository/CORepository.java
+++ server/src/main/java/net/guides/springboot2/crud/repository/CORepository.java
@@ -12,24 +12,26 @@ import net.guides.springboot2.crud.model.CO;
 @Repository
 public interface CORepository extends JpaRepository<CO, Integer> {
 
-	List<CO> findByContract_Id(int idcontract);
+	public List<CO> findByContract_Id(int idcontract);
 
-	List<CO> findByContract_IdOrderByDelaDesc(int idcontract);
+	public List<CO> findByContract_IdOrderByDelaDesc(int idcontract);
 
-	List<CO> findByContract_IdAndTip(int idcontract, String tip);
+	public List<CO> findByContract_IdAndTip(int idcontract, String tip);
 
 	@Query(value = "SELECT c FROM CO c WHERE c.contract.id = ?1 AND c.tip = ?2 AND YEAR(c.dela) = ?3")
-	List<CO> findByContract_IdAndTipAndYear(int idcontract, String tip, int an);
+	public List<CO> findByContract_IdAndTipAndYear(int idcontract, String tip, int an);
 
-	List<CO> findByContract_IdAndTipOrderByDelaDesc(int idcontract, String tip);
+	public List<CO> findByContract_IdAndTipOrderByDelaDesc(int idcontract, String tip);
 
-	CO findByTipAndDelaAndPanalaAndContract_Id(String tip, LocalDate dela, LocalDate panala, int idcontract);
+	public CO findByTipAndDelaAndPanalaAndContract_Id(String tip, LocalDate dela, LocalDate panala, int idcontract);
 
-	List<CO> findAllByOrderByDelaAsc();
+	public List<CO> findByContract_Angajat_Societate_Id(int idsocietate);
+
+	public List<CO> findAllByOrderByDelaAsc();
 
 	@Query(value = "SELECT c FROM CO c WHERE YEAR(c.dela) = ?1 AND MONTH(c.dela) = ?2")
-	List<CO> findByAnAndLuna(int an, int luna);
+	public List<CO> findByAnAndLuna(int an, int luna);
 
 	@Query(value = "SELECT c FROM CO c WHERE YEAR(c.dela) = ?1")
-	List<CO> findByAn(int an);
+	public List<CO> findByAn(int an);
 }
